d09a23c836e426feecf966a1aa347645ef6a3690
古德祯 <gudz@shengruan.com>
2020-03-25 14:45:11 +0800

    test1

---
+++ src/Pat.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/**
+ * Created by EG on 2020/3/25.
+ */
+public class Pat {
+    static void pat1(){
+        System.out.println("1001 A+B Format (20 goals)");
+        System.out.println("input 2 integer a,b(-10^-6 <= a,b <= 10^6)");
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.printf("%,d",a+b);
+    }
+
+    static void pat2(){
+        System.out.println("1001 A+B Format (20 goals)");
+    }
+
+}
